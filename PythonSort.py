3481624c06cb6d87b2007a2e75e669688f3936ff
a71143055 <a71143055@gmail.com>
2024-10-29 14:52:07 +0900

    PythonSort

--- PythonSort.py
+++ PythonSort.py
@@ -1,3 +1,13 @@
 li = [8,7,3,4,9,10,5,6,1,2]
 li = sorted(li)
 print(li)
+
+scores = []
+
+for i in range(10):
+    score = int(input(f"{i + 1}번 학생의 점수를 입력해주세요 : "))
+    scores.append(score)
+
+scores = sorted(scores)
+
+print(f"내림차순 정렬 결과 : {scores}")
