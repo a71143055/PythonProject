6ac8128fee2d7f2b425802f52bd84c7df8f015f0
a71143055 <a71143055@gmail.com>
2024-11-18 13:20:04 +0900

    PythonProject

--- DollPickUp.py
+++ DollPickUp.py
@@ -3,10 +3,11 @@ import random as rand
 
 class DollPickUp:
     Dolls = {
-        Doll("피까츄", "수컷", 5000, "medium"),
-        Doll("바비인형", "여성", 7500, "tall"),
-        Doll("카봇저글링", "수컷", 6400, "small")
+        1 : Doll("피까츄", "수컷", 5000, "medium"),
+        2 : Doll("바비인형", "여성", 7500, "tall"),
+        3 : Doll("카봇저글링", "수컷", 6400, "small")
     }
 
-    def pickUp(self,num):
+    def pickUp(self,num : int):
         if rand.randint(100) <= 30:
+            print(f'{}')
