ea387e16d7d41bf5509c8975aa842cd0a92d8763
a71143055 <a71143055@gmail.com>
2024-11-18 12:02:49 +0900

    PythonProject

---
+++ CarMain.py
@@ -0,0 +1,6 @@
+from Truck import Truck
+
+tr = Truck('Mighty', 3900,'white', 2500)
+tr.load(700)
+tr.info()
+tr.run()
--- Truck.py
+++ Truck.py
@@ -2,7 +2,7 @@ from Car import Car
 
 class Truck(Car):
     def __init__(self, model, engine, color='blue', cap=5000):
-        Car.__init__(self.model, engine,color)
+        Car.__init__(self, model, engine,color)
         self.capacity = cap
         self.loading=0
     def load(self,w):
