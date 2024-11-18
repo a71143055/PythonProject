9c307187c06da30a34c239356679b2c5aa7c5d2d
a71143055 <a71143055@gmail.com>
2024-11-18 11:47:23 +0900

    PythonProject

---
+++ Truck.py
@@ -0,0 +1,14 @@
+from Car import Car
+
+class Truck(Car):
+    def __init__(self, model, engine, color='blue', cap=5000):
+        Car.__init__(self.model, engine,color)
+        self.capacity = cap
+        self.loading=0
+    def load(self,w):
+        self.loading+=w
+    def info(self):
+        super(Truck,self.info())
+        Car.info(self)
+        print(f'Capacity : {self.capacity/1000 : .1f} ton')
+        print(f'Current Loading : {self.loading} kg')
