d478bdfd59fe57ab409099e0968fa50aa6bfefbf
a71143055 <a71143055@gmail.com>
2024-11-22 09:55:54 +0900

    PythonProject

--- main1122.py
+++ main1122.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 d = pd.DataFrame({
     'date' : ['2019-01-03', '2021-11-22', '2023-01-05'],
@@ -48,3 +50,28 @@ df.loc[3,'C2'] = np.nan
 print(df)
 df = df.dropna()
 print(df)
+
+a = ['A','B','A','C','C','A','B','A','D','D','A','D']
+a = pd.Series(a)
+a_dict = {'A':1,'B':2,'C':3,'D':4}
+b = a.map(a_dict)
+p = pd.DataFrame({'변환 전' : a, '변환 후' : b})
+print(p)
+
+df = pd.DataFrame(
+    np.random.randn(8,3),
+    columns = ['C1','C2','C3'])
+df.loc[2,'C2'] = 1
+df.loc[3,'C2'] = -10
+
+plt.boxplot([df['C1'],df['C3']])
+plt.show()
+
+df = pd.DataFrame(
+    np.random.randn(8,3),
+    columns = ['C1','C2','C3'])
+df.loc[2,'C2'] = 1
+df.loc[3,'C2'] = -10
+
+sns.heatmap(df.corr(), annot=True)
+plt.show()
