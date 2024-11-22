225e7ccf5b0abb69581bf84fe74946ac920c6604
a71143055 <a71143055@gmail.com>
2024-11-22 11:36:10 +0900

    PythonProject

--- main1122.py
+++ main1122.py
@@ -75,3 +75,55 @@ df.loc[3,'C2'] = -10
 
 sns.heatmap(df.corr(), annot=True)
 plt.show()
+
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('./data/medical.csv')
+print(df.head())
+
+print(df.columns)
+
+df.rename(columns={'Hipertension':'Hypertension','Handcap':'Handicap'},inplace=True)
+print(df.columns)
+
+print(df.info())
+
+print(df.isnull().any(axis=1))
+print(df.isnull().any(axis=0))
+
+print(df.describe())
+
+df = df[df.Age >= 0]
+print(df.Age.min())
+
+df = df[(df.Handicap==0) | (df.Handicap==1)]
+print(df['Handicap'].value_counts())
+
+df['AppointmentDay'] = pd.to_datetime(df['AppointmentDay'])
+df['ScheduledDay'] = pd.to_datetime(df['ScheduledDay'])
+print(df.info())
+
+df['waiting_day'] = df['AppointmentDay'].dt.dayofyear - df['ScheduledDay'].dt.dayofyear
+print(df.info())
+print(df.describe())
+
+df = df[df.waiting_day>=0]
+print(df['waiting_day'].min())
+
+print(df.Age.unique())
+
+df = df[df.Age <= 110]
+plt.figure(figsize=(16,2))
+sns.boxplot(x=df.Age)
+plt.show()
+
+a = df[df.waiting_day==0]['waiting_day'].value_counts()
+b = df[(df['waiting_day']==0) & (df['No-show']==1)]['waiting_day'].value_counts()
+print(b/a)
+
+no_show = df[df['No-show']==1]
+show = df[df['No-show']==0]
+
+no_show[no_show['waiting_day']<=10]['waiting_day'].hist(alpha=0.7, label='no_show')
+show[show['waiting_day']<=10]['waiting_day'].hist(alpha=0.3, label='show')
+plt.legend()
+plt.show()
